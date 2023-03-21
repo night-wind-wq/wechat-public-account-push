4046678b827770369787be1beece52920aac9690
night-wind-wq <82702024+night-wind-wq@users.noreply.github.com>
2023-03-21 22:08:29 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -33,7 +33,7 @@ const USER_CONFIG = {
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '李四', year: '2001', date: '07-31',
+          type: '生日', name: '宝宝', year: '2001', date: '07-31',
         },
         {
           type: '节日', name: '相识纪念日', year: '2023', date: '02-22',
